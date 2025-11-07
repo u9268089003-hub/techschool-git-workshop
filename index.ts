02731eaa778ba8e0649209a1be60d2f7f6f94394
Emil <emiing22@o365.mrvgs.no>
2025-11-07 10:14:44 +0100

    Bug fix at index.ts 1:1

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
-export const greeting = (name: string) => {
-    console.log(`Hello ${name}`);
+/**
+ * Function that greets a person
+ * @param firstname First name
+ * @param lastname Last name
+ */
+
+export const greeting = (firstname: string, lastname: string) => {
+    console.log(`Hello ${firstname} ${lastname}`);
 };
